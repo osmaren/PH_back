9714705ac3675d8c33049d289571475317a7d5fd
Osmar Eduardo Nothaft <osmarn@gmail.com>
2023-03-02 08:47:09 -0300

    Add - CORS configuration

--- src/routes/ticket.route.js
+++ src/routes/ticket.route.js
@@ -12,6 +12,12 @@ import { validateUnitMaxOccupancy } from "../middlewares/validateUnitMaxOccupanc
 const router = Router();
 
 export const ticketRoute = (app) => {
+  router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  });
   router.get("", ListAllTicketsController.index);
   router.post("", validateCreateTicket, validateUnitMaxOccupancy, CreateTicketController.create);
   router.get("/people", ListPeoplePerUnitController.list);
